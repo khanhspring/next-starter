f2937cf44cbb394a8bc6336180fac7eb2869af2e
Harry Tran <harry.tran@avancevl.com>
2024-02-03 07:10:24 +0700

    Update SignIn page

--- src/components/pages/SignIn/SignIn.tsx
+++ src/components/pages/SignIn/SignIn.tsx
@@ -1,26 +1,29 @@
 'use client';
 
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 
 export default function SignIn() {
   const searchParams = useSearchParams();
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="w-[350px] p-10 flex flex-col items-center gap-5">
-        <h1 className="font-bold uppercase text-lg">Sign In</h1>
-        <button
-          type="button"
-          onClick={() =>
-            signIn('google', {
-              callbackUrl: searchParams.get('callbackUrl') || '/',
-            })
-          }
-          className="px-5 py-2 bg-blue-500 rounded"
-        >
-          Sing In with Google
-        </button>
+    <Suspense>
+      <div className="flex h-screen items-center justify-center">
+        <div className="w-[350px] p-10 flex flex-col items-center gap-5">
+          <h1 className="font-bold uppercase text-lg">Sign In</h1>
+          <button
+            type="button"
+            onClick={() =>
+              signIn('google', {
+                callbackUrl: searchParams.get('callbackUrl') || '/',
+              })
+            }
+            className="px-5 py-2 bg-blue-500 rounded"
+          >
+            Sing In with Google
+          </button>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
