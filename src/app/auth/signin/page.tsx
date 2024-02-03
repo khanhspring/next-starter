6330044ef6d8c1af35c0929f531a1f8d19d53e16
Harry Tran <harry.tran@avancevl.com>
2024-02-03 07:05:24 +0700

    Update SignIn page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,26 +1,5 @@
-'use client';
+import SignIn from '@/components/pages/SignIn/SignIn';
 
-import { useSearchParams } from 'next/navigation';
-import { signIn } from 'next-auth/react';
-
-export default function SignIn() {
-  const searchParams = useSearchParams();
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="w-[350px] p-10 flex flex-col items-center gap-5">
-        <h1 className="font-bold uppercase text-lg">Sign In</h1>
-        <button
-          type="button"
-          onClick={() =>
-            signIn('google', {
-              callbackUrl: searchParams.get('callbackUrl') || '/',
-            })
-          }
-          className="px-5 py-2 bg-blue-500 rounded"
-        >
-          Sing In with Google
-        </button>
-      </div>
-    </div>
-  );
+export default function SignInPage() {
+  return <SignIn />;
 }
